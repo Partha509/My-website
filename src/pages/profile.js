5a84142288c156988c4523872b992dfd9b718336
HP <parthapartha509@gmail>
2025-08-20 01:17:03 +0600

    change

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,7 @@ function profile() {
   return (
     <div className="content">
       <h2>Profile</h2>
-      <p><b>Full Name:</b> Partha Sarathi Saha</p>
+      <p><b>Full Name:</b> Partha Sarathi Saha Tirtha</p>
       <p><b>Email:</b> partha.cse.20230104017@aust.edu</p>
       <p><b>Date of Joining:</b> August 17, 2025</p>
     </div>
